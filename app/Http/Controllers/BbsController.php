95f0297e89617aec1d09597ed138fcec3cbbc304
Anastasia Kulbina <kulbina@logema.org>
2024-04-19 12:18:53 +0300

    Изменены пути, добавлен шаблон, добалено лого

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Bb;
 
 class BbsController extends Controller
 {
-    public function index(){
-        return response("Здусь будет перечень объявлений")
-            ->header('Content-Type', 'text/plain');
+    public function index()
+    {
+        $context = ['bbs' => Bb::latest()->get()];
+        return view('index', $context);
+    }
+
+    public function detail(Bb $bb)
+    {
+        return view('detail', ['bb'=>$bb]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\BbsController::class, 'index']);;
+Route::get('/', [\App\Http\Controllers\BbsController::class, 'index'])->name('index');
+Route::get('/{bb}', [\App\Http\Controllers\BbsController::class, 'detail'])->name('detail');
